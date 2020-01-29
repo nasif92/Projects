c118c3b362f38703480dd3595836b9a84ae8fddd
Nasif <nhossain@ualberta.ca>
2020-01-29 16:38:04 -0700

    Doing binary search and linaer search in c++

--- C++ and Arduino/binary_search_in_c++/binary_Search.cpp
+++ C++ and Arduino/binary_search_in_c++/binary_Search.cpp
@@ -1,55 +1,93 @@
 #include <iostream>
 
 using namespace std;
-int
-binarySearch (const int value, const int *A, const int N)
-{
-  // TODO: Implement a binary search that returns
-  // the index where all values in A are less than
-  // the given value.
-  int mid = (N / 2) - 1;
-  int right = N;
-  int left = 0;
-  int index = 0;
-  bool found = false;
-  if ((value < A[0]) || (value > A[N - 1]))
-    {
-      index = -1;
-    }
-  else
-    {
-      index = -1;
-      while ((left != right) && (!found))
-{
- if (A[mid] == value)
-   {
-     index = mid;
-     found = true;
-   }
- else if (A[mid] > value)
-   {
-     right = mid;
-     mid = (left + right - 1) / 2;
-   }
- else if (A[mid] < value)
-   {
-     left = mid + 1;
-     mid = (left + right - 1) / 2;
-   }
 
-}
-    }
-  return index;
+int binarySearch(const int value, const int *A, const int N){
+	// TODO: Implement a binary search that returns
+	// the index where all values in A are less than
+	// the given value.
+  // i is left and j is right
+	int left = 0; 
+  int right = N - 1; 
+  int mid;
+  if (value < A[0]){
+		return 0;
+	}
+	else if (value > A[N - 1]){
+		return N;
+	}
+  else{
+    while (left <= right){
+        mid = (left + right) / 2;
+        if (A[mid] < value){
+          left = mid + 1;
+        }
+        else{
+          right = mid - 1;
+        }
+	  }
+  }
+	
 }
 
-int
-main ()
+int linearSearch(const int value, const int *A, const int N)
+{
+	// TODO: Implement a sequential search that returns
+	// the index where all values in A are less than
+	// the given value.
+	int i = 0;
+	while (*(A + i) < value){
+		i += 1;
+	}
+
+	return i;
+}
+
+int main ()
 {
   cout << "Hello World" << endl;
-  int arr[] = { 1, 2, 3, 4, 5, 6 };
-  int *p = arr;
-  int n = binarySearch (4, p, 6);
-  cout << n << endl;
+  int A[] = { 1, 1, 1, 1, 1, 2, 2, 2, 2, 2,3,3,3,3,5};
+  int B[] = {1, 1, 1, 1, 1, 2, 2, 2, 2, 2,3,3,3,3,5};
+  int C[30] ;
+  int x =0;
+  int y = 0;
+  int N = 15;
+  // linear search here
+  for (int i = 0; i < N; i++){
+		x = linearSearch(A[i] , B, N);
+    C[x + i ] = A[i];
+    // cout << "X: " << x << endl;
+    y = linearSearch(B[i]+ 1 , A, N);
+    // cout << "Y: " << y << endl;
+
+    C[y + i ] = B[i];
+	}
+  
+  // cout << "Time required for linear search: " << duration.count() << endl;
+ 
+  for ( int i = 0; i < 20; i++){
+      cout << "Ouput: ";
+      cout << C[i] << endl;
+    }
+     // binary search here
+ 
+  
+  for (int i = 0; i < N; i++){
+		x = binarySearch(A[i] , B, N);
+    C[x + i ] = A[i];
+    // cout << "X: " << x << endl;
+    y = binarySearch(B[i]+ 1 , A, N);
+    // cout << "Y: " << y << endl;
+
+    C[y + i ] = B[i];
+	}
+  // cout << "Time required for binary search: " << duration.count() << endl;
+ 
+  for ( int i = 0; i < 20; i++){
+    cout << "Ouput: ";
+    cout << C[i] << endl;
+  }
+  
 
   return 0;
 }
